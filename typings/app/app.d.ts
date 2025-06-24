d8a8b30bcb02cd49ff94a41256c374dcc6a18827
林嘉伦 <linjialun@kingsware.cn>
2025-06-24 16:34:50 +0800

    fix: 补充类型

--- typings/app/app.d.ts
+++ typings/app/app.d.ts
@@ -1,3 +1,28 @@
 declare namespace App {
   type test = string
+
+  type LayoutMode = 'aside' | 'top' | 'top-aside'
+  interface LayoutConfig {
+    asideWidth: number
+    headerHeight: number
+    footerHeight: number
+    tabHeight: number
+    gap: number
+    asideCollapse: boolean
+    asideCollapseWidth: number
+    showTabs: boolean
+    showBreadcrumb: boolean
+    showFooter: boolean
+  }
+
+  interface BaseLayoutConfig extends Omit<LayoutConfig, 'asideCollapse' | 'asideCollapseWidth'> {
+    layoutMode: LayoutMode
+  }
+
+  interface BaseConfig {
+    layout: BaseLayoutConfig
+    theme: {
+      primaryColor: string
+    }
+  }
 }
